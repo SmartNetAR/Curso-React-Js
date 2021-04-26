b8b201fefe5ab043fca0b354d5b7f9c15a6906e2
yurleider1219 <yurleider1219@gmail.com>
2021-04-26 18:39:21 -0300

    cambio variable

--- primer-app/src/components/DateComponent.js
+++ primer-app/src/components/DateComponent.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function DateComponent(props){
     
-    let fechaInicioAño = new Date("2021/1/1");
+    let fechaInicioAnio = new Date("2021/1/1");
 
     let aFecha1 = props.fecha.split('/');
     let fFecha1 = Date.UTC(aFecha1[2],aFecha1[1]-1,aFecha1[0]);
     
-    let resta = (  fFecha1 - fechaInicioAño.getTime() );
+    let resta = (  fFecha1 - fechaInicioAnio.getTime() );
 
     let diasRestantes = Math.round( resta / (1000 * 60 * 60 * 24));
  
