57fc58a5b034be0109a8fc265c5f44cbc612a0ac
yurleider1219 <yurleider1219@gmail.com>
2021-04-26 21:16:42 -0300

    clase numero 3

--- primer-app/src/components/DateComponent.js
+++ primer-app/src/components/DateComponent.js
@@ -2,22 +2,25 @@ import React from "react";
 
 function DateComponent(props){
     
-    let fechaInicioAnio = new Date("2021/1/1");
+    const dias = (fechaCalcular)=>{
+        let fechaInicioAnio = new Date("2021/1/1");
 
-    let aFecha1 = props.fecha.split('/');
-    let fFecha1 = Date.UTC(aFecha1[2],aFecha1[1]-1,aFecha1[0]);
+        let aFecha1 = props.fecha.split('/');
+        let fFecha1 = Date.UTC(aFecha1[2],aFecha1[1]-1,aFecha1[0]);
+        
+        let resta = (  fFecha1 - fechaInicioAnio.getTime() );
     
-    let resta = (  fFecha1 - fechaInicioAnio.getTime() );
-
-    let diasRestantes = Math.round( resta / (1000 * 60 * 60 * 24));
+        return Math.round( resta / (1000 * 60 * 60 * 24)).toString();
+    }
  
     return(
         <div>
 
-            <h1>Pasaron { diasRestantes.toString() } del inicio del año hasta hoy </h1>
+            <h1>Pasaron { dias(props.fecha) } del inicio del año hasta hoy </h1>
             
         </div>
     );
 }
+
 
 export default DateComponent;
